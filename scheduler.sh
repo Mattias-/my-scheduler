191678b3915212f36eabe3ca0a63ceed504f898d
Mattias Appelgren <mattias@ppelgren.se>
2019-09-12 21:12:28 +0200

    Use kubectl --raw

--- scheduler.sh
+++ scheduler.sh
@@ -1,53 +1,54 @@
 #!/bin/bash
 set -euo pipefail
 
-API_URL="https://$(minikube ip):8443/api/v1"
-BINDING_PAYLOAD='{
-  "apiVersion": "v1",
-  "kind": "Binding",
-  "metadata": {"name": "PODNAME"},
-  "target": {"apiVersion": "v1", "kind": "Node", "name": "NODENAME"}
-}'
-
-get_pods() {
+get_pending_pods() {
     scheduler=$1
     kubectl get pods -ojson \
         --field-selector="status.phase=Pending,spec.schedulerName=$scheduler" |
-        jq -r '.items[].metadata | "\(.namespace) \(.name)"'
+        jq --compact-output '.items[]'
 }
 
-get_nodes() {
-    kubectl get node -ojson |
-        jq -r '.items[].metadata.name'
+get_node() {
+    # TODO Choose node with care
+    kubectl get node -ojson | jq -r '.items | first | .metadata.name'
 }
 
 bind_pod() {
     node=$1
     namespace=$2
     name=$3
-    echo "$BINDING_PAYLOAD" |
-        jq ".metadata.name = \"$name\" | .target.name = \"$node\"" |
-        curl -sk \
-            -X POST \
-            -H "Content-Type: application/json" \
-            --cert "$HOME/.minikube/client.crt" \
-            --key "$HOME/.minikube/client.key" \
-            --url "${API_URL}/namespaces/${namespace}/pods/${name}/binding" \
-            -d @-
+    kubectl create \
+        --raw "/api/v1/namespaces/${namespace}/pods/${name}/binding" -f - <<EOF
+{
+  "apiVersion": "v1",
+  "kind": "Binding",
+  "metadata": {
+    "name": "$name"
+  },
+  "target": {
+    "apiVersion": "v1",
+    "kind": "Node",
+    "name": "$node"
+  }
+}
+EOF
 }
 
 schedule_pod() {
-    namespace=$1
-    pod=$2
-    # TODO Choose node with care
-    node=$(get_nodes | head -1)
+    pod=$1
+    namespace=$(jq -r '.metadata.namespace' <<<"$pod")
+    name=$(jq -r '.metadata.name' <<<"$pod")
+    node="$(get_node)"
     bind_pod "$node" "$namespace" "$pod"
 }
 
-while true; do
-    get_pods "my-scheduler" | while read np; do
-        echo "$np"
-        schedule_pod $np
+main() {
+    while true; do
+        get_pending_pods "my-scheduler" | while read -r pod; do
+            schedule_pod "$pod"
+        done
+        sleep 10
     done
-    sleep 10
-done
+}
+
+main
